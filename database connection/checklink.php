f6f7a1fbb1c45cbd963a1ef100a7e1b3fdc6e420
shixuan Wang <62128097+ptk7218@users.noreply.github.com>
2020-05-22 13:08:06 +1200

    check connection with cmslamp14 host

---
+++ database connection/checklink.php
@@ -0,0 +1,19 @@
+ <?php
+
+ require_once('sqlinfo.inc.php');
+
+  $conn = @mysqli_connect($sql_host,
+		$sql_user,
+		$sql_pass,
+		$sql_db);
+	
+	// Checks if connection is successful
+	if (!$conn) {
+		// Displays an error message
+		echo "<p>Database connection failure</p>";
+	}
+	else {
+		echo "<p>success</p>";
+	}	
+
+?>
---
+++ database connection/sqlinfo.php
@@ -0,0 +1,12 @@
+<?php
+
+// Include file with sql details
+// please change to your user, password, and db
+// please put it under /home/<user>/conf/
+
+	$sql_host="cmslamp14.aut.ac.nz";
+	$sql_user=" "; //e.g. ptk7218
+	$sql_pass=" ";
+	$sql_db=" "; //e.g. ptk7218
+//	$sql_tble=" "; select which table u want to link
+?>
