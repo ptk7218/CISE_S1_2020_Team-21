8ad6bfaefca894bf77fed8e986623eea9d51f11b
shixuan Wang <62128097+ptk7218@users.noreply.github.com>
2020-05-25 21:36:05 +1200

    update

--- index.php
+++ index.php
@@ -19,34 +19,8 @@
     </head>
 
     <body>
-        <header>
-            <!-- logo -->
-            <div class ="logo">
-                <h1 class = "logo-text"><span>SEER</span>Search</h1>
-            </div>
-
-            <!-- navigation bar-->
-            <!-- phone mode-->
-            <!-- <i class="fas fa-align-justify menu-toggle"></i> -->
-            <ul class= "nav">
-                <li><a href="http://">Home</a></li>
-                <li><a href="http://">About</a></li>
-                <li><a href="http://">Services</a></li>
-                <!--<li><a href="http://">Sign Up</a></li>
-                <li><a href="http://">Login</a></li>-->
-                <li>
-                    <a href="http://">
-                        <i class="far fa-address-card"></i>
-                        Shixuan Wang
-                        <i class="fas fa-angle-down"></i>
-                    </a>
-                    <ul>
-                        <li><a href="http://">Dashboard</a></li>
-                        <li><a href="http://" class = "Logout">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </header>
+     
+    <?php include("app/header.php"); ?>
     
     
             <p class = "intro">
--- login.php
+++ login.php
@@ -19,34 +19,9 @@
     </head>
 
     <body>
-        <header>
-            <!-- logo -->
-            <div class ="logo">
-                <h1 class = "logo-text"><span>SEER</span>Search</h1>
-            </div>
+        
+    <?php include("app/header.php"); ?>
 
-            <!-- navigation bar-->
-            <!-- phone mode-->
-            <!-- <i class="fas fa-align-justify menu-toggle"></i> -->
-            <ul class= "nav">
-                <li><a href="http://">Home</a></li>
-                <li><a href="http://">About</a></li>
-                <li><a href="http://">Services</a></li>
-                <!--<li><a href="http://">Sign Up</a></li>
-                <li><a href="http://">Login</a></li>-->
-                <li>
-                    <a href="http://">
-                        <i class="far fa-address-card"></i>
-                        Shixuan Wang
-                        <i class="fas fa-angle-down"></i>
-                    </a>
-                    <ul>
-                        <li><a href="http://">Dashboard</a></li>
-                        <li><a href="http://" class = "Logout">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </header>
     
       <div class="auth-content">
 
@@ -65,7 +40,7 @@
          </div> 
 
         <div>
-            <button type="submit" name="login-btn" class="btn btn-big">Register</button>
+            <button type="submit" name="login-btn" class="btn btn-big">login</button>
         </div>
         <p>Or <a href="register.html">Sign Up</a></p>
 
