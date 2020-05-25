63e405c37e510772b7749a572c90265a9221299c
shixuan Wang <62128097+ptk7218@users.noreply.github.com>
2020-05-25 21:42:31 +1200

    update footer.php and header.php

---
+++ includes/footer.php
@@ -0,0 +1,50 @@
+<div class= "footer">
+        <div class="footer-content">
+
+            <div class = "footer-section about">
+                <h1 class="logo-text"><span>SEER</span>Search</h1>
+                <p>short introduction</p>
+
+                <div class= "contact">
+                    <span><i class="fas fa-phone"></i>&nbsp; 0800-12341234</span>
+                    <span><i class="fas fa-envelope"></i>&nbsp; info@autuni.ac.nz</span>
+                </div>
+                
+                <div class="socials">
+                    <a href="#"><i class="fab fa-facebook-square"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+
+            <div class="footer-section links">
+                <h2>Quick Links </h2>
+                <br>
+                <ul>
+                    <a href="#"><li>Help</li></a>
+                    <a href="#"><li>Developer Team</li></a>
+                    <a href="#"><li>Privacy Policy</li></a>
+                    <a href="#"><li>Blog</li></a>
+                    <a href="#"><li>Subscribe</li></a>
+                </ul>
+
+
+            </div>
+            <div class="footer-section contact-form">
+                <h2>Contact us</h2>
+                <br>
+                <form action="index.html" method ="post">
+                    <input type ="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+                    <textarea name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+                    <button type="submit" class="btn btn-big">
+                        <i class="fas fa-envelope"></i>
+                        Send
+                    </button>
+                </form>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            &copy; SEERSearch | Designed by SE Stream14 Group 21
+        </div>
+    </footer>
--- login.php
+++ login.php
@@ -19,8 +19,8 @@
     </head>
 
     <body>
-        
-    <?php include("app/header.php"); ?>
+
+    <?php include("includes/header.php"); ?>
 
     
       <div class="auth-content">
@@ -49,45 +49,8 @@
           
 
     <!--footer-->
-    <div class= "footer">
-        <div class="footer-content">
+    <?php include("includes/footer.php"); ?>
 
-            <div class = "footer-section about">
-                <h1 class="logo-text"><span>SEER</span>Search</h1>
-                <p>short introduction</p>
-
-                <div class= "contact">
-                    <span><i class="fas fa-phone"></i>&nbsp; 0800-12341234</span>
-                    <span><i class="fas fa-envelope"></i>&nbsp; info@autuni.ac.nz</span>
-                </div>
-
-                <div class="socials">
-                    <a href="#"><i class="fab fa-facebook-square"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                    <a href="#"><i class="fab fa-youtube"></i></a>
-                </div>
-            </div>
-
-            <div class="footer-section links">
-                <h2>Quick Links </h2>
-                <br>
-                <ul>
-                    <a href="#"><li>Help</li></a>
-                    <a href="#"><li>Developer Team</li></a>
-                    <a href="#"><li>Privacy Policy</li></a>
-                    <a href="#"><li>Blog</li></a>
-                    <a href="#"><li>Subscribe</li></a>
-                </ul>
-
-
-            </div>
-            
-        </div>
-        <div class="footer-bottom">
-            &copy; SEERSearch | Designed by SE Stream14 Group 21
-        </div>
-    </footer>
     </main>
 </body>
 </html>
