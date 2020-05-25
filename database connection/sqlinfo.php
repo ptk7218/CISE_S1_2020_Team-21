74754ca8a930aca4fe2f676a66d6944023669f24
shixuan Wang <62128097+ptk7218@users.noreply.github.com>
2020-05-25 19:43:57 +1200

    Update sqlinfo.php

--- database connection/sqlinfo.php
+++ database connection/sqlinfo.php
@@ -4,8 +4,19 @@
 
 
 	$sql_host="cmslamp14.aut.ac.nz";
-	$sql_user=" "; //e.g. ptk7218
-	$sql_pass=" ";
-	$sql_db=" "; //e.g. ptk7218
+	$sql_user="ptk7218"; //e.g. ptk7218
+	$sql_pass="62217wsxl"; // ur password
+	$sql_db="ptk7218"; //e.g. ptk7218
 //	$sql_tble=" "; select which table u want to link
+
+
+$conn - new MySQLi($sql_host, $sql_user, $sql_pass, $sql_db);
+
+
+if($conn->connect_error){
+	die('Database connection error:' . $conn->connect_error);
+}
+else{
+	echo 'connection success';
+}
 ?>
