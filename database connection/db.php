f81682df3e55add1dc86414936cda2411b3ccc44
shixuan Wang <62128097+ptk7218@users.noreply.github.com>
2020-05-30 03:38:23 +1200

    update

--- database connection/db.php
+++ database connection/db.php
@@ -3,10 +3,10 @@
 require('sqlinfo.php');
 
 $sql = "SELECT * FROM seerusers";
-$stmt = $conn -> prepare($sql);
-$stmt -> execute();
-$users = $stmt -> get_result()->fetch_all(MYSQLI_ASSOC);
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$users = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
 var_dump($seerusers);
 
-?>
+
