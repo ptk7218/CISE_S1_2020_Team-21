f14fa329670dd1bf6aee775842f88d903ac1f14c
shixuan Wang <62128097+ptk7218@users.noreply.github.com>
2020-05-30 04:30:11 +1200

    update

--- register.php
+++ register.php
@@ -24,7 +24,26 @@ include("database connection/sqlinfo.php");
     <body>
     <!-- header -->
     <?php include("includes/header.php"); ?>
-    <?php include("config.php") ?>
+    <?php 
+    if(isset($_POST['register-btn'])){
+
+       $username = $_POST['username'];
+       $fname = $_POST['fname'];
+       $lname = $_POST['lname'];
+       $affiliation = $_POST['radio'];
+       $email = $_POST['email'];
+       $password = $_POST['password'];
+       
+       $sql = "INSERT INTO seerusers (username, fname, lname, radio, email, password) values(?, ?, ?, ?, ?)";
+       $stmtinsert = $sql_db->prepare($sql);
+       $result = $stmtinsert->execute([$username, $fname, $lname, $affiliation, $email, $password]);
+       if($result){
+         echo 'successfully saved';
+       }else{
+         echo 'there are errors while saving the data.';
+       }
+    }
+?>
     
       <div class="auth-content">
 
