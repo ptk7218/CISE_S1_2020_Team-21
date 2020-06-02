5a2dd83efb6943b199aeb7c18526c87e95188c15
shixuan Wang <62128097+ptk7218@users.noreply.github.com>
2020-06-02 18:47:53 +1200

    update

--- registration_code.php
+++ registration_code.php
@@ -3,17 +3,17 @@ Here, we write code for registration.
 -->
 <?php
 require_once('connection.php');
-$username = $fname = $lname = $radio = $email = $password = $pwd = '';
+$username = $fname = $lname = $radio = $email = $password1 = $pwd = '';
 
 $username = $_POST['username'];
 $fname = $_POST['firstname'];
 $lname = $_POST['lastname'];
 $radio = $_POST['radio'];
 $email = $_POST['email'];
-$pwd = $_POST['password'];
-$password = MD5($pwd);
+$pwd = $_POST['password1'];
+$password1 = MD5($pwd);
 
-$sql = "INSERT INTO tbluser (Firstname,Lastname,Affiliation,Email,Password) VALUES ('$fname','$lname','$radio','$email','$password')";
+$sql = "INSERT INTO tbluser (Firstname,Lastname,Affiliation,Email,Password) VALUES ('$fname','$lname','$radio','$email','$password1')";
 $result = mysqli_query($conn, $sql);
 if($result)
 {
